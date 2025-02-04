4a39aef3ed35605bd5ed7e629c6dabaec44f9a6b
Akinsulire Olabode Solomon <solomonakinsulire@gmail.com>
2025-02-04 12:20:55 +0000

    feat: Import Trustees component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import Trustees from "./components/Trustees";
 
 export default function Home() {
   return (
     <div>
       <main>
         <Hero />
+        <Trustees />
       </main>
     </div>
   );
