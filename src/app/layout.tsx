c9e4777a060ed341e9223fc14df0e07f4851c5b3
Akinsulire Olabode Solomon <solomonakinsulire@gmail.com>
2025-02-04 22:23:47 +0000

    feat:Removed unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import {  Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Suspense } from "react";
 import Loader from "./components/Loader";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const poppins = Poppins({
   variable: "--poppins",
@@ -35,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
+        className={`${poppins.variable} antialiased`}
       >
         <Suspense fallback={<Loader />}>
           <Navbar />
